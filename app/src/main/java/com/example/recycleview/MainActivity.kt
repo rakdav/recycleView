2f93f54daac5ac37057b70be6513f75a5554dcfc
su <rakdav@inbox.ru>
2022-03-17 13:24:47 +0200

    2

--- app/src/main/java/com/example/recycleview/MainActivity.kt
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.recycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recView=findViewById<RecyclerView>(R.id.listCountries)
+        recView.layoutManager=LinearLayoutManager(this)
+        var data=ArrayList<ItemsViewModel>()
+        data.add(ItemsViewModel("Аргентина","Буэнос-Айрос",R.drawable.argentina))
+        data.add(ItemsViewModel("Бразилия","Бразилиа",R.drawable.brazl))
+        data.add(ItemsViewModel("Чили","Сантьяго",R.drawable.flag_of_chile))
+        data.add(ItemsViewModel("Уругвай","Монтевидео",R.drawable.urugvai))
+        data.add(ItemsViewModel("Колумбия","Богота",R.drawable.co))
+        val adapter=CountryAdapter(data)
+        recView.adapter=adapter
     }
 }
